0825cf33d60c3549470634831fd437ea6a421712
Deep Monapara <deepmonapara0@gmail.com>
2024-11-25 23:54:14 +0530

    update backend configuration to include .env in .gitignore and connect to MongoDB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,20 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import 'dotenv/config';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("connected to the db"));
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get("/test", async (req: Request, res: Response) => {
-   res.json({ message: "Hello World" });
-})
+  res.json({ message: "Hello World" });
+});
 
 app.listen(7000, () => {
-   console.log("server is running on port 7000");
-})
+  console.log("server is running on port 7000");
+});
