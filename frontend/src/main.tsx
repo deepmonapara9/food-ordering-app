93d2f9a5f41074eb4e13acc1a92715d354981a69
Deep Monapara <deepmonapara0@gmail.com>
2024-11-27 00:14:50 +0530

    Add Auth0 authentication integration; update MainNav for login functionality and wrap AppRoutes with Auth0Provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom'
-import AppRoutes from './AppRoutes'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
+import Auth0ProviderNavigate from "./auth/Auth0ProviderNavigate";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-      <AppRoutes />
+      <Auth0ProviderNavigate>
+        <AppRoutes />
+      </Auth0ProviderNavigate>
     </Router>
-  </StrictMode>,
-)
+  </StrictMode>
+);
