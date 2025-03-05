78173d79822a8ab5d56715b33a9470c7b8600111
Deep Monapara <deepmonapara0@gmail.com>
2025-03-05 19:33:21 +0530

    Remove unnecessary handleSave function from UserProfilePage; directly use updateUser for form submission

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -4,11 +4,6 @@ import UserProfileForm from '@/forms/user-profile-form/UserProfileForm'
 const UserProfilePage = () => {
   const { updateUser, isLoading} = useUpdateMyUser();
 
-  const handleSave = async (formData: any) => {
-    console.log("Saving user with data:", formData); // ✅ Debugging log
-    await updateUser(formData);
-  }
-
   return (
     <UserProfileForm onSave={updateUser} isLoading={isLoading}/>
   );
